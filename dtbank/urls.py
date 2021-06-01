87479dd9472fb331119c597686b7b6f88a3a9863
kiymetakdemir <21kymet@gmail.com>
2021-06-01 17:34:14 +0300

    login authentication and password encryption

--- dtbank/urls.py
+++ dtbank/urls.py
@@ -3,18 +3,18 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='dtbank-home'),
-    path('login/', views.userloginpage, name='dtbank-login'),
+    path('', views.home, name='home'),
+    path('createtables', views.createtables, name='createtables'),
+    path('login/', views.userloginpage, name='login'),
     path('managerlogin/', views.managerlogin, name='managerlogin'),
     path('userdirect/', views.login, name="userdirect"),
     path('managerloginpage/', views.managerloginpage, name='managerloginpage'),
     path('managerdirect/', views.managerlogin, name="managerdirect"),
     path('managerhome/', views.managerhome, name="managerhome"),
-    path('userhome/', views.userhome, name="userhome")
-    
-    #path('createtables/', views.my_custom_sql, name = "createtables")
-    #path('adduser/', views.adduser, name='dtbank-adduser')
-    #path('userlogin/', views.userlogin, name='dtbank-userlogin'),
-    #path('managerlogin/', views.managerlogin, name='dtbank-managerlogin')
+    path('userhome/', views.userhome, name="userhome"),
+    path('saveuser/', views.saveuser, name='saveuser'),
+    path('adduser/', views.adduser, name = 'adduser'),
+    path('encrypt/', views.encrypt_passwords, name='encrypt')
+
 
 ]
--- dtbank/views.py
+++ dtbank/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 import django.contrib.auth.hashers as hasher
 from django.shortcuts import redirect
 from django.db import connection
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
 # Create your views here.
 
 def home(request):
@@ -18,47 +21,89 @@ def managerloginpage(request):
 
 def login(request):
 	username = request.POST.get('username')	#check if username exists in database
-	encoded = ""	#takes the value of password of username
+	institution = request.POST.get('institution')
 	password = request.POST.get('password')
-	#if hasher.check_password(password, encoded): #check database manager - password
-	if hasher.check_password(password, encoded):
+
+	cursor = connection.cursor()
+	query = "select password from User_Work where username='"+username+"' and institution_name='"+institution+"'"
+	cursor.execute(query)
+	encodedtuple = cursor.fetchall()
+	if len(encodedtuple)==0:
+		return render(request, 'login.html', {'message': "Invalid username or password!"})
+
+	encoded = encodedtuple[0][0]		#check if there is any value
+
+	if hasher.check_password(password, encoded):	#check database user - password
 		return render(request, 'userhome.html', {'username':username})
 	else:
 		return render(request, 'login.html', {'message': "Invalid username or password!"})
 
 def managerlogin(request):
 	username = request.POST.get('username')	#check if username exists in database
-	encoded = ""	#takes the value of password of username
 	password = request.POST.get('password')
 	#if hasher.check_password(password, encoded): #check database manager - password
+	cursor = connection.cursor()
+	query = "select password from Database_Manager where username='"+username+"'"
+	cursor.execute(query)
+	encodedtuple = cursor.fetchall()
+	if len(encodedtuple)==0:
+		return render(request, 'managerlogin.html', {'message': "Invalid username or password!"})
+		
+	encoded = encodedtuple[0][0]		#check if there is any value
+
 	if hasher.check_password(password, encoded):
-		return render(request, 'managerhome.html', {'username':username})
+		return render(request, 'managerhome.html', {'message':'Welcome '+username+"!"})
 	else:
 		return render(request, 'managerlogin.html', {'message': "Invalid username or password!"})
 
 def userhome(request, username):
-	return render(request, 'userhome.html', {'username':username})
+	return render(request, 'userhome.html', {'message':'Welcome '+username+"!"})
 
 def managerhome(request, username):
-	return render(request, 'managerhome.html', {'username':username})
+	return render(request, 'managerhome.html', {'message':'Welcome '+username+"!"})
 
+def createtables(request):
+	with connection.cursor() as c:
+    		c.execute("CREATE TABLE User ( password	CHAR(30), username	CHAR(20) NOT NULL, institution CHAR(100), PRIMARY KEY(username,institution))")
 
-"""def adduser(request):
-	form = UserCreationForm(request.POST)
-	if form.is_valid():
-		username = form.cleaned_data.get('username')
-		username = form.cleaned_data.get('password')
-		messages.success(request, f'Account created for {username}!')
-		return redirect('dtbank-adduser')
-	else:
-		form = UserCreationForm()
-	return render(request, 'dtbank/adduser.html', {'form': form})"""
-
-"""def my_custom_sql(request):
+def saveuser(request):
+	username = request.POST.get('username')
+	name = request.POST.get('name')
+	institution = request.POST.get('institution')
+	password = request.POST.get('password')
+	encoded = hasher.make_password(password, hasher='pbkdf2_sha256')		#save the username, institution name and the encrypted to database
 	cursor = connection.cursor()
-	cursor.execute("select name from demo")
-	names = cursor.fetchall()
-	return render(request, 'dtbank/home.html', {'names':names})"""
+	insertion = "insert into User_Work values ('"+encoded+"','"+name+"','"+username+"','"+institution+"')"
+	cursor.execute(insertion)
+	return render(request,'managerhome.html', {'message':"User saved successfully!"})
+
+def adduser(request):
+	return render(request, 'adduser.html')
+
+def encrypt_passwords(request):
+	cursor = connection.cursor()			#encode database manager passwords
+	cursor.execute("select username, password from Database_Manager")
+	user_password = cursor.fetchall()
+
+	for username, password in user_password:
+		encoded = hasher.make_password(password)
+		query = "update Database_Manager set password='"+encoded+"' where username='"+username+"'"
+		cursor.execute(query)
+
+	cursor.execute("select username, institution_name, password from User_Work")	#encode user passwords
+	user_password = cursor.fetchall()
+
+	for username, institution, password in user_password:
+		encoded = hasher.make_password(password)
+		query = "update User_Work set password='"+encoded+"' where username='"+username+"' and institution_name= '"+institution+"'"
+		cursor.execute(query)
+
+	return redirect('home')
+
+
+
+
+
 
 
 
